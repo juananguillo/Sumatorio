1feb93ba443f0350cce257c722189eb6f0657bf9
Juan <juananguillo@gmail.com>
2020-03-04 20:45:58 +0100

    Preparacion

---
+++ Tests/sumador/TestAsumar.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package sumador;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Juan
+ *
+ */
+class TestAsumar {
+
+	@Test
+	void test() {
+		fail("No implementado aun");
+	}
+
+}
